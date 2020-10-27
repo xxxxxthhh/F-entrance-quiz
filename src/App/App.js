3af7d4e8a77708d383c87f32ff9c32158d770f0b
KyleXu <kyle_x@foxmail.com>
2020-10-27 19:52:13 +0800

    [XTH] add Navi

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,22 @@
+/*
+ * @Author: Kyle Xu
+ * @Date: 2020-10-27 18:17:55
+ * @LastEditors: Kyle Xu
+ * @LastEditTime: 2020-10-27 19:46:01
+ * @Blog: http://kylexu.cn
+ * @Github: https://github.com/xxxxxthhh
+ * @Mail: kyle_x@foxmail.com
+ * @Description:  // write sth about Code
+ */
 import React, { Component } from 'react';
 import './App.scss';
+import Navigator from '../Components/Navigator';
 
 class App extends Component {
   render() {
     return (
       <div data-testid="app" className="App">
-        Hello World
+        <Navigator />
       </div>
     );
   }
---
+++ src/Components/Navigator.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: Kyle Xu
+ * @Date: 2020-10-27 19:28:11
+ * @LastEditors: Kyle Xu
+ * @LastEditTime: 2020-10-27 19:47:12
+ * @Blog: http://kylexu.cn
+ * @Github: https://github.com/xxxxxthhh
+ * @Mail: kyle_x@foxmail.com
+ * @Description:  // write sth about Code
+ */
+import React, { Component } from 'react';
+
+class Navigator extends Component {
+  render() {
+    return (
+      <div className="navigator">
+        <div>
+          <span>分组列表</span>
+        </div>
+        <div>
+          <button type="button">学员分组</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navigator;
